60c3f4717d7c16a9eb6cc712f628d2c4fd09c348
Amanda <achen3592@bths.edu>
2022-10-26 19:15:49 -0400

    update

--- src/SelectionMadness.java
+++ src/SelectionMadness.java
@@ -1,25 +1,22 @@
-public class SelectionMadness
-{
+public class SelectionMadness {
     // No instance variables //
 
     // no-parameter constructor with "empty" body since no instance variables
-    public SelectionMadness() { }
+    public SelectionMadness() {
+    }
 
     /* Simulates randomly flipping a fair coin and returning true for heads and
        false for tails; hint: this method should use the Math.random() method to
        generate a random number.  This method returns true (heads) 50% of the
        time and false (tails) 50% of the time, on average.
      */
-    public boolean flipCoin()
-    {
+    public boolean flipCoin() {
         boolean result = true;
         int coin = (int) (Math.random() * 2) + 1;
-        if (coin == 1)
-        {
+        if (coin == 1) {
             return result;
         }
-        if (coin == 2)
-        {
+        if (coin == 2) {
             return result;
         }
         return result;
@@ -28,16 +25,18 @@ public class SelectionMadness
 
     /* Returns the largest of three provided integers: num1, num2, or num3
      */
-    public int largest(int num1, int num2, int num3)
-    {
-        if ((num1 > num2) == (num1 > num3)){
-            return num1;
-        } else if ((num2 > num3) == (num2 > num3)) {
-            return num2;
-        } else if ((num3 > num1) == (num3 > num2)) {
-            return num3;
+    public int largest(int num1, int num2, int num3) {
+        int largerNum;
+        if (num1 > num2) {
+            largerNum = num1;
+        } else {
+            largerNum = num2;
+        }
+        if (num3 > largerNum){
+            largerNum = num3;
+        }
+        return largerNum;
     }
-
 
 
     /* Returns true if the three provided lengths create a right triangle, in
@@ -48,10 +47,19 @@ public class SelectionMadness
        side3 = 3 -- will your code work regardless of which side is the longest
        of the three?
      */
-    public boolean rightTriangle(int side1, int side2, int side3)
-    {
+        public boolean rightTriangle ( int side1, int side2, int side3){
+            int largeNum = largest(side1, side2, side3);
 
-    }
-
+            boolean answer = true;
+            if (largeNum == side1) {
+                answer = (largeNum == Math.sqrt(Math.pow(side2, 2) + Math.pow(side3, 2)));
+            } else if (largeNum == side2) {
+                answer = (largeNum == Math.sqrt(Math.pow(side1, 2) + Math.pow(side3, 2)));
+            } else {
+                answer = (largeNum == Math.sqrt(Math.pow(side2, 2) + Math.pow(side1, 2)));
+            }
+            return answer;
+        }
 }
+
 
